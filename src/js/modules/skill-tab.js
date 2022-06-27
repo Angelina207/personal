7eab29cf5a70ac5d05c987ed7fb1ccd79cb5bf8f
Angelina <75496111+Angelina207@users.noreply.github.com>
2022-06-27 21:26:13 +0300

    tab-js

--- src/js/modules/skill-tab.js
+++ src/js/modules/skill-tab.js
@@ -1,3 +1,19 @@
 export function skillTab() {
 
+    const tabContent = document.querySelector('.skill-tab');
+
+    tabContent.addEventListener('click', showTab);
+
+    function showTab() {
+        const tabElem = document.querySelectorAll('skillTab');
+
+        for (let i = 0; i > tabElem.length; i++) {
+
+            if (tabElem[i] === event.target) {
+                tabElem[i].classList.add('tab-is-active');
+            } else {
+                tabElem[i].classList.remove('tab-is-active');
+            }
+        }
+    }
 }
